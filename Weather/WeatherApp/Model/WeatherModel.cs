1e08291a500e47a33f20cf80e63a9d1616fa7f2a
kristoffer <kristoffer9464@hotmail.com>
2020-11-09 11:22:36 +0100

    database

---
+++ Weather/WeatherApp/Model/WeatherModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+namespace WeatherApp.Model
+{
+    public class WeatherModel
+    {
+        
+    }
+
+    public class Weather : INotifyPropertyChanged
+    {
+        private string condition;
+
+        public string Condition
+        {
+            get { return condition; }
+
+            set { if (condition != value)
+                {
+                    condition = value;
+                    RaisePropertyChanged("Condition");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ Weather/WeatherApp/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherViewModel
+    {
+        public WeatherViewModel()
+        {
+        }
+    }
+}
